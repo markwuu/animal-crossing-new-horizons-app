afed8fcf5cfb30032a7402651613d335fe788c67
Mark W <markwuu@gmail.com>
2026-01-13 13:13:09 -0800

    added logic to display fish

--- app/fishlist.tsx
+++ app/fishlist.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect, FC } from 'react';
 import axios from 'axios';
+import { times } from './lib/definitions';
 
 interface ChildProps {
 	monthNumber: number;
@@ -9,11 +10,14 @@ interface ChildProps {
 }
 
 export const FishList: FC<ChildProps> = ({ monthNumber, localTime }) => {
-	console.log(`🚀 ~ FishList ~ localTime:`, localTime);
-	console.log(`🚀 ~ FishList ~ monthNumber:`, monthNumber);
-	const [fish, setFish] = useState<[]>([]);
 	const apiKey = '';
-	console.log(`🚀 ~ FishList ~ fish:`, fish);
+	const monthIndex = monthNumber + 1;
+	const [fish, setFish] = useState([]);
+	const fourAMtoNinePM = times.slice(4, 21);
+	const nineAMtofourPM = times.slice(9, 16);
+	const ninePMtoElevenPM = times.slice(21, 24);
+	const twelveAMtoFourAM = times.slice(0, 4);
+	const ninePMtoFourAM = ninePMtoElevenPM.concat(twelveAMtoFourAM);
 
 	useEffect(() => {
 		const fetchFish = async () => {
@@ -22,11 +26,14 @@ export const FishList: FC<ChildProps> = ({ monthNumber, localTime }) => {
 					headers: {
 						'X-API-Key': apiKey,
 						'Content-Type': 'application/json',
-						'Access-Control-Allow-Origin': '*',
-						'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
 					},
 				});
-				setFish(response.data);
+				const fish = response.data.map((fish) => {
+					fish.display = true;
+					fish.checked = false;
+					return fish;
+				});
+				setFish(fish);
 			} catch (err) {
 				console.log('an error occurred', err);
 			}
@@ -36,28 +43,61 @@ export const FishList: FC<ChildProps> = ({ monthNumber, localTime }) => {
 	}, []);
 
 	const handleClick = (filter: string) => {
-		console.log(`🚀 ~ handleClick ~ filter:`, filter);
+		if (filter === 'all') {
+			setFish((prev) => {
+				return prev?.map((fish) => {
+					fish.display = true;
+					return fish;
+				});
+			});
+		}
 
-		//filter by current month
-		const newFish = fish?.map((fish) => {
-			if (fish['north']['times_by_month'][1] !== 'NA') {
-				return fish['name'], fish['north']['times_by_month'][1];
-			}
-		});
-		console.log('newFish', newFish);
+		if (filter === 'month') {
+			setFish((prev) => {
+				return prev?.map((fish) => {
+					fish.display = true;
+					const availableTimes = fish['north']['times_by_month'][monthIndex];
+					if (availableTimes === 'NA') fish.display = false;
+					return fish;
+				});
+			});
+		}
 
-		// setFish((prev) => {
-		//   prev.map((fish) => {
-		// 		if (fish['north']['times_by_month'][1] !== 'NA') {
-		// 			return fish['name'], fish['north']['times_by_month'][1];
-		// 		}
-		// 	});
-		// });
+		if (filter === 'current') {
+			setFish((prev) => {
+				return prev?.map((fish) => {
+					fish.display = false;
+					const availableTimes = fish['north']['times_by_month'][monthIndex];
+					if (availableTimes === 'NA') fish.display = false;
+					if (availableTimes === 'All day') fish.display = true;
+					if (
+						availableTimes === '4 AM – 9 PM' &&
+						fourAMtoNinePM.includes(localTime)
+					) {
+						fish.display = true;
+					}
+					if (
+						availableTimes === '9 AM – 4 PM' &&
+						nineAMtofourPM.includes(localTime)
+					) {
+						fish.display = true;
+					}
+					if (
+						availableTimes === '9 PM – 4 AM' &&
+						ninePMtoFourAM.includes(localTime)
+					) {
+						fish.display = true;
+					}
+
+					return fish;
+				});
+			});
+		}
 	};
 
-	//filters: all, monthly, today, current
 	return (
-		<>
+		<div>
+			<h2 className="text-center text-2xl pb-2">Fish</h2>
 			<div className="flex justify-center gap-5">
 				<button
 					onClick={() => handleClick('all')}
@@ -80,17 +120,19 @@ export const FishList: FC<ChildProps> = ({ monthNumber, localTime }) => {
 			</div>
 			<div className="pt-6 grid grid-cols-4">
 				{fish?.map((fish) => {
-					return (
-						<div
-							className="flex items-center border-2 border-white px-3 py-3"
-							key={fish['name']}
-						>
-							<input type="checkbox" className={`h-5 w-5`} />
-							<p className="pl-2 text-sm">{fish['name']}</p>
-						</div>
-					);
+					if (fish.display === true) {
+						return (
+							<div
+								className="flex items-center border-2 border-white px-3 py-3"
+								key={fish['name']}
+							>
+								<input type="checkbox" className={`h-5 w-5`} />
+								<p className="pl-2 text-sm">{fish['name']}</p>
+							</div>
+						);
+					}
 				})}
 			</div>
-		</>
+		</div>
 	);
 };
