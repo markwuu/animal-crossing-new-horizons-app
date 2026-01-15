7fb60aae912e5971dec83b6c61720b70fdbec4a8
Mark W <markwuu@gmail.com>
2026-01-14 20:50:18 -0800

    updated styles for header

--- app/page.tsx
+++ app/page.tsx
@@ -21,12 +21,12 @@ export default function Home() {
 	}).format(date);
 
 	return (
-		<div className="flex min-h-screen justify-center bg-zinc-50 font-sans dark:bg-black">
-			<main className="flex min-h-screen w-full max-w-3xl flex-col py-15 px-4 bg-white dark:bg-black">
-				<h1 className="flex flex-col pb-2 text-center text-2xl uppercase font-bold tracking-tight font-mono">
+		<div className="flex min-h-screen justify-center bg-zinc-50 font-sans dark:bg-black border-0 border-white">
+			<main className="flex min-h-screen w-full max-w-2xl md:max-w-3xl flex-col py-15 px-4 bg-white dark:bg-black border-0 border-white">
+				<h1 className="flex flex-col pb-2 text-center text-2xl uppercase font-bold tracking-tight font-mono border-0 border-white">
 					Animal Crossing New Horizons: Fish
 				</h1>
-				<p className="pb-2 text-center text-2xl">
+				<p className="pb-2 text-center text-lg">
 					{monthName}, {localTime}
 				</p>
 				<FishList monthNumber={Number(monthIndex)} localTime={localTimeHour} />
